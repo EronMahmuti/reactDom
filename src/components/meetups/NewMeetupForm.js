7e318d71deb6a216249f2f5b4600abd85ac95382
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-01-06 15:25:21 +0100

    meetup form css

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,31 @@
+import classes from './NewMeetupForm.module.css';
+import Card from '../ui/Card';
+
+function NewMeetupForm(rops){
+    return (
+        <Card >
+            <form className={classes.form} >
+                <div className={classes.control} >
+                    <label htmlFor='title' >Meetup Title </label>
+                    <input type="text"  reqcuired id='title' />
+                </div>
+                <div className={classes.control} >
+                    <label htmlFor='image' >Meetup Title </label>
+                    <input type="url"  required id="image" />
+                </div>
+                <div className={classes.control} >
+                    <label htmlFor='address' >Address </label>
+                    <input type="text"  required id="address" />
+                </div>
+                <div className={classes.control} >
+                    <label htmlFor='description' >description</label>
+                    <textarea  required id="description" rows="5" />
+                </div>
+                
+            </form>
+
+        </Card>
+    );
+}
+
+export default NewMeetupForm;
