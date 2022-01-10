d99c73d2562bc46f527ab0992c9f01f8c432bed3
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-01-10 17:21:34 +0100

    dummy data deleted

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -2,33 +2,24 @@ import { useState } from "react/cjs/react.development";
 import MeetupList from "../components/meetups/MeetupList";
 
 
-const Dummy_data = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      },
-      {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      },
-];
-
 
 function AllMeetupsPage(){
   const [isLoading, setIsLoading ] = useState(true);
+  const [ loadedMeetups, setLoadedMeetups] =   useState();
+
   fetch('https://react-getting-started-533aa-default-rtdb.firebaseio.com/meetups.json').then((response) => {
     return response.json();
-  })
+  }).then((data) => {
+    setIsLoading(false);
+  });
+
+  if(isLoading){
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
     return (
         <section>
