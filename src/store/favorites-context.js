5fe4e5acc81d2cf3a33ba32af69fa993451d6261
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-01-11 02:00:35 +0100

    Favorites functional comp

---
+++ src/store/favorites-context.js
@@ -0,0 +1,50 @@
+import {createContext} from 'react';
+
+const FavoritesContext = createContext({
+    favorites: [],
+    totalFavorites: 0,
+    addFavorite: (favoriteMeetup) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetupId) => {}
+});
+
+function FavoritesContextProvider(props){
+    cont [userFavorites, setUserFavorites] = useState([]);
+
+    function addFavoriteHandler(favoriteMeetup) {
+        setUserFavorites((prevUserFavorites) => {
+            return prevUserFavorites.concat(favoriteMeetup);
+        });
+    }
+    function removeFavoriteHandler(meetupId){
+        setUserFavorites(prevUserFavorites => {
+            return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
+        });
+    }
+    function itemIsFavoriteHandler(meetupId){
+        return userFavorites.some(meetup => meetup.id === meetupId );
+    }
+
+    const context = {
+        favorites: userFavorites,
+        totalFavorites: userFavorites.length,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavoriteHandler,
+        itemIsFavorite: itemIsFavoriteHandler
+    };
+
+    return <FavoritesContext.Provider value={context} >
+        { props.children }
+    </FavoritesContext.Provider>
+}
+
+
+
+
+
+
+
+
+
+
+
